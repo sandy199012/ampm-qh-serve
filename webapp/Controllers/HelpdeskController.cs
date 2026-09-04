5df1206306611454f2ffe198e0bebca44db82314
Sandy <snd.sandeepan@gmail.com>
2026-09-04 17:00:17 +0530

    Final fix: Cookie auth, no session, all refs cleaned

--- webapp/Controllers/HelpdeskController.cs
+++ webapp/Controllers/HelpdeskController.cs
@@ -59,7 +59,7 @@ public class HelpdeskController : Controller
             ["assignedTo"] = form["assignedTo"].ToString(),
             ["status"]     = "Open",
             ["dateRaised"] = DateTime.Now.ToString("yyyy-MM-dd HH:mm"),
-            ["raisedBy"]   = HttpContext.Session.GetString("name")
+            ["raisedBy"]   = User.FindFirst("FullName")?.Value
         };
         _db.SaveTicket(ticket);
         TempData["Success"] = "Ticket created: " + ticket["ticketId"];
