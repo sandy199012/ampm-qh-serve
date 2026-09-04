dbe2799cfccf232e7cce38ef41c5af30cda8eec8
Sandy <snd.sandeepan@gmail.com>
2026-09-04 23:26:39 +0530

    Fix: Disable antiforgery + ephemeral DataProtection

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,11 +1,23 @@
 using AMPMWeb.Data;
 using AMPMWeb.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews(options => {
-    options.Filters.Add(new IgnoreAntiforgeryTokenAttribute());
-}).AddRazorRuntimeCompilation();
+
+// Disable antiforgery completely
+builder.Services.AddControllersWithViews(o => 
+    o.Filters.Add(new IgnoreAntiforgeryTokenAttribute()));
+
+// Ephemeral data protection - no disk needed
+builder.Services.AddDataProtection()
+    .UseEphemeralDataProtectionProvider();
+
+// Disable antiforgery service
+builder.Services.AddAntiforgery(o => {
+    o.SuppressXFrameOptionsHeader = true;
+    o.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.None;
+});
 
 builder.Services.AddSingleton<DbService>();
 builder.Services.AddSingleton<AuthService>();
@@ -14,11 +26,11 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
-var dbService = app.Services.GetRequiredService<DbService>();
+
 var dbPath = Environment.GetEnvironmentVariable("DB_PATH")
     ?? System.IO.Path.Combine(AppContext.BaseDirectory, "Data", "ampm.db");
 SeedDb.RestoreIfEmpty(dbPath);
-dbService.Init();
+app.Services.GetRequiredService<DbService>().Init();
 
 app.UseStaticFiles();
 app.UseRouting();
